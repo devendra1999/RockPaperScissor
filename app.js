cd114f0a7fdba1efe4f4f6f3f7611c984d764693
Devendra Singh <devendrasamant8505@gmail.com>
2024-07-23 01:51:06 +0530

    update: updated required functionality, consoled logic

--- app.js
+++ app.js
@@ -3,8 +3,39 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genComputerChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return options[randomIndex];
+}       
+
+const playGame = (userChoice) => {
+    //
+    console.log("player chosen: ", userChoice);
+    const compChoice = genComputerChoice();
+    console.log("computer chose: ", compChoice);
+
+    if (userChoice === compChoice) {
+        console.log("It's a tie!");
+    } else if (
+        (userChoice === "rock" && compChoice === "scissors") ||
+        (userChoice === "paper" && compChoice === "rock") ||
+        (userChoice === "scissors" && compChoice === "paper")
+    ) {
+        console.log("Player wins!");
+        userScore++;
+    } else {
+        console.log("Computer wins!");
+        compScore++;
+    }
+}
+
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
-        console.log("choice clicked");
+        // event generation on click and event handling on click is executing the function
+        // console.log("choice clicked");
+        const userChoice = choice.id;
+        playGame(userChoice);
+        // showWinner(userChoice, compChoice);
     })
 })
